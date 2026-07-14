0614e4f0c9cc33619fe07196d3658828a4d9d795
Dmitry Shapovalov <shenf@mail.ru>
2026-07-14 12:48:21 +0300

    Правило EBNF

---
+++ src/main/java/ru/d_shap/lr1/ebnf/EbnfRule.java
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.ebnf;
+
+/**
+ * The EBNF rule.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class EbnfRule implements EbnfNode {
+
+    private final String _name;
+
+    private final EbnfNode _expression;
+
+    /**
+     * Create new object.
+     *
+     * @param name       the name of the EBNF rule.
+     * @param expression the expression of the EBNF rule.
+     */
+    public EbnfRule(final String name, final EbnfNode expression) {
+        super();
+        _name = name;
+        _expression = expression;
+    }
+
+    /**
+     * Get the name of the EBNF rule.
+     *
+     * @return the name of the EBNF rule.
+     */
+    public String getName() {
+        return _name;
+    }
+
+    /**
+     * Get the expression of the EBNF rule.
+     *
+     * @return the expression of the EBNF rule.
+     */
+    public EbnfNode getExpression() {
+        return _expression;
+    }
+
+}
