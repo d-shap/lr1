5147aade318d231e7792234ce096a7ea79327697
Dmitry Shapovalov <shenf@mail.ru>
2026-07-14 13:16:35 +0300

    опциональный элемент EBNF

---
+++ src/main/java/ru/d_shap/lr1/ebnf/EbnfOptional.java
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.ebnf;
+
+/**
+ * The EBNF optional.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class EbnfOptional implements EbnfNode {
+
+    private static final long serialVersionUID = 1L;
+
+    private final EbnfNode _expression;
+
+    /**
+     * Create new object.
+     *
+     * @param expression the expression of the EBNF optional.
+     */
+    public EbnfOptional(final EbnfNode expression) {
+        super();
+        _expression = expression;
+    }
+
+    /**
+     * Get the expression of the EBNF optional.
+     *
+     * @return the expression of the EBNF optional.
+     */
+    public EbnfNode getExpression() {
+        return _expression;
+    }
+
+}
