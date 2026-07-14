f8c5097fae6d04756ab5cd8fc9e1b287a5d11ed4
Dmitry Shapovalov <shenf@mail.ru>
2026-07-14 14:56:51 +0300

    Добавил заголовок, javadoc, отформатировал

--- src/main/java/ru/d_shap/lr1/ebnf/EbnfTokenType.java
+++ src/main/java/ru/d_shap/lr1/ebnf/EbnfTokenType.java
@@ -1,5 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.lr1.ebnf;
 
+/**
+ * The EBNF token type.
+ *
+ * @author Dmitry Shapovalov
+ */
 enum EbnfTokenType {
 
     IDENTIFIER,
@@ -7,17 +31,23 @@ enum EbnfTokenType {
     STRING,
 
     EQUALS,
+
     COMMA,
+
     PIPE,
+
     SEMICOLON,
 
     LPAREN,
+
     RPAREN,
 
     LBRACKET,
+
     RBRACKET,
 
     LBRACE,
+
     RBRACE,
 
     EOF
