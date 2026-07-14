7efe382fd835025b6f00cce866437d146c7251b4
Dmitry Shapovalov <shenf@mail.ru>
2026-07-14 18:06:51 +0300

    Класс ошибки

---
+++ src/main/java/ru/d_shap/lr1/ebnf/EbnfException.java
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.ebnf;
+
+import ru.d_shap.lr1.ParserException;
+
+/**
+ * The EBNF exception.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class EbnfException extends ParserException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create new object.
+     *
+     * @param message the message.
+     */
+    protected EbnfException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param message the message.
+     * @param cause   the cause.
+     */
+    protected EbnfException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+}
