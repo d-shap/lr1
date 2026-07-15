e16b8239141aceaae57344167196c082d1067bd5
Dmitry Shapovalov <shenf@mail.ru>
2026-07-15 09:05:30 +0300

    Сгенерированный парсер

---
+++ src/main/java/ru/d_shap/lr1/ebnf/EbnfTokenizer.java
@@ -0,0 +1,284 @@
+/// ////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+/// ////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.ebnf;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The EBNF tokenizer.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class EbnfTokenizer {
+
+    private final Reader _reader;
+
+    private int _currentChar;
+
+    private int _nextChar;
+
+    private int _line;
+
+    private int _column;
+
+    private boolean _eof;
+
+    /**
+     * Create a new tokenizer for the specified reader.
+     *
+     * @param reader the reader to tokenize.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public EbnfTokenizer(final Reader reader) throws IOException {
+        super();
+        _reader = reader;
+        _line = 1;
+        _column = 1;
+        _eof = false;
+        readNextChar();
+        readNextChar();
+    }
+
+    /**
+     * Tokenize the input and return a list of tokens.
+     *
+     * @return the list of tokens.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public List<EbnfToken> tokenize() throws IOException {
+        List<EbnfToken> tokens = new ArrayList<>();
+        while (!isAtEnd()) {
+            skipWhitespaceAndComments();
+            if (isAtEnd()) {
+                break;
+            }
+            EbnfToken token = nextToken();
+            tokens.add(token);
+        }
+        tokens.add(new EbnfToken(EbnfTokenType.EOF, "", _line, _column));
+        return tokens;
+    }
+
+    private EbnfToken nextToken() throws IOException {
+        int startLine = _line;
+        int startColumn = _column;
+        char currentChar = peek();
+
+        // Single-character tokens
+        if (currentChar == '=') {
+            advance();
+            return new EbnfToken(EbnfTokenType.EQUALS, "=", startLine, startColumn);
+        }
+        if (currentChar == ',') {
+            advance();
+            return new EbnfToken(EbnfTokenType.COMMA, ",", startLine, startColumn);
+        }
+        if (currentChar == '|') {
+            advance();
+            return new EbnfToken(EbnfTokenType.PIPE, "|", startLine, startColumn);
+        }
+        if (currentChar == ';') {
+            advance();
+            return new EbnfToken(EbnfTokenType.SEMICOLON, ";", startLine, startColumn);
+        }
+        if (currentChar == '(') {
+            advance();
+            return new EbnfToken(EbnfTokenType.LPAREN, "(", startLine, startColumn);
+        }
+        if (currentChar == ')') {
+            advance();
+            return new EbnfToken(EbnfTokenType.RPAREN, ")", startLine, startColumn);
+        }
+        if (currentChar == '[') {
+            advance();
+            return new EbnfToken(EbnfTokenType.LBRACKET, "[", startLine, startColumn);
+        }
+        if (currentChar == ']') {
+            advance();
+            return new EbnfToken(EbnfTokenType.RBRACKET, "]", startLine, startColumn);
+        }
+        if (currentChar == '{') {
+            advance();
+            return new EbnfToken(EbnfTokenType.LBRACE, "{", startLine, startColumn);
+        }
+        if (currentChar == '}') {
+            advance();
+            return new EbnfToken(EbnfTokenType.RBRACE, "}", startLine, startColumn);
+        }
+
+        // String tokens (quoted with single or double quotes)
+        if (currentChar == '"' || currentChar == '\'') {
+            return parseString(currentChar, startLine, startColumn);
+        }
+
+        // Identifier tokens (letters, digits, underscores)
+        if (isIdentifierStart(currentChar)) {
+            return parseIdentifier(startLine, startColumn);
+        }
+
+        throw new EbnfException("Unexpected character: '" + currentChar + "' at line " + startLine + ", column " + startColumn);
+    }
+
+    private EbnfToken parseString(final char quoteChar, final int startLine, final int startColumn) throws IOException {
+        advance(); // skip opening quote
+        StringBuilder text = new StringBuilder();
+        while (!isAtEnd() && peek() != quoteChar) {
+            if (peek() == '\\') {
+                advance();
+                if (!isAtEnd()) {
+                    char escaped = peek();
+                    switch (escaped) {
+                        case 'n':
+                            text.append('\n');
+                            break;
+                        case 't':
+                            text.append('\t');
+                            break;
+                        case 'r':
+                            text.append('\r');
+                            break;
+                        case '\\':
+                            text.append('\\');
+                            break;
+                        case '"':
+                            text.append('"');
+                            break;
+                        case '\'':
+                            text.append('\'');
+                            break;
+                        default:
+                            text.append(escaped);
+                    }
+                    advance();
+                }
+            } else {
+                text.append(peek());
+                advance();
+            }
+        }
+        if (isAtEnd()) {
+            throw new EbnfException("Unterminated string at line " + startLine + ", column " + startColumn);
+        }
+        advance(); // skip closing quote
+        return new EbnfToken(EbnfTokenType.STRING, text.toString(), startLine, startColumn);
+    }
+
+    private EbnfToken parseIdentifier(final int startLine, final int startColumn) throws IOException {
+        StringBuilder text = new StringBuilder();
+        while (!isAtEnd() && isIdentifierPart(peek())) {
+            text.append(peek());
+            advance();
+        }
+        return new EbnfToken(EbnfTokenType.IDENTIFIER, text.toString(), startLine, startColumn);
+    }
+
+    private void skipWhitespaceAndComments() throws IOException {
+        while (!isAtEnd()) {
+            char currentChar = peek();
+            if (currentChar == ' ' || currentChar == '\t' || currentChar == '\r') {
+                advance();
+            } else if (currentChar == '\n') {
+                advance();
+                _line++;
+                _column = 1;
+            } else if (currentChar == '/' && peekNext() == '/') {
+                // Skip line comment
+                advance();
+                advance();
+                while (!isAtEnd() && peek() != '\n') {
+                    advance();
+                }
+            } else if (currentChar == '/' && peekNext() == '*') {
+                // Skip block comment
+                advance();
+                advance();
+                while (!isAtEnd() && !(peek() == '*' && peekNext() == '/')) {
+                    if (peek() == '\n') {
+                        _line++;
+                        _column = 1;
+                        advance();
+                    } else {
+                        advance();
+                    }
+                }
+                if (!isAtEnd()) {
+                    advance(); // skip '*'
+                    advance(); // skip '/'
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    private char peek() {
+        if (isAtEnd()) {
+            return '\0';
+        }
+        return (char) _currentChar;
+    }
+
+    private char peekNext() {
+        if (isAtEnd()) {
+            return '\0';
+        }
+        return (char) _nextChar;
+    }
+
+    private void advance() throws IOException {
+        if (!isAtEnd()) {
+            if (_currentChar == '\n') {
+                _line++;
+                _column = 1;
+            } else {
+                _column++;
+            }
+            _currentChar = _nextChar;
+            readNextChar();
+        }
+    }
+
+    private void readNextChar() throws IOException {
+        int ch = _reader.read();
+        if (ch == -1) {
+            _nextChar = -1;
+        } else {
+            _nextChar = ch;
+        }
+    }
+
+    private boolean isAtEnd() {
+        return _currentChar == -1;
+    }
+
+    private boolean isIdentifierStart(final char ch) {
+        return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_';
+    }
+
+    private boolean isIdentifierPart(final char ch) {
+        return isIdentifierStart(ch) || ch >= '0' && ch <= '9';
+    }
+
+}
