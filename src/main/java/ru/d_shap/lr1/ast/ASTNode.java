05af6a303d40c8f3706f79686741938006381ab6
Dmitry Shapovalov <shenf@mail.ru>
2026-07-23 16:52:23 +0300

    Реализация AST

---
+++ src/main/java/ru/d_shap/lr1/ast/ASTNode.java
@@ -0,0 +1,53 @@
+/// ////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+/// ////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.ast;
+
+/**
+ * Базовый интерфейс для любого узла AST.
+ * Работает с любой грамматикой без привязки к конкретным правилам.
+ */
+public interface ASTNode {
+
+    /**
+     * Получить тип узла (имя правила или тип токена).
+     */
+    String getType();
+
+    /**
+     * Получить значение узла (для терминалов - текст токена).
+     */
+    Object getValue();
+
+    /**
+     * Принять визитор для обхода дерева.
+     */
+    <T> T accept(ASTVisitor<T> visitor);
+
+    /**
+     * Получить количество дочерних узлов.
+     */
+    int getChildCount();
+
+    /**
+     * Получить дочерний узел по индексу.
+     */
+    ASTNode getChild(int index);
+
+}
