b0cead185397f7fbb82f2334c986918e6eabc2ca
Dmitry Shapovalov <shenf@mail.ru>
2026-07-28 17:47:54 +0300

    Убрал пробелы

--- src/test/java/ru/d_shap/lr1/TestRunner.java
+++ src/test/java/ru/d_shap/lr1/TestRunner.java
@@ -119,7 +119,7 @@ public final class TestRunner {
 
         GrammarTokenizer grammarTokenizer = new GrammarTokenizer();
         grammarTokenizer.initializeFromGrammar(grammarMap, allProductions);
-        List<Token> tokens = grammarTokenizer.tokenize("21 + 3 ^ 2 / sin(11.2 * 0.21)");
+        List<Token> tokens = grammarTokenizer.tokenize("21+3^2/sin(11.2*0.21)");
         System.out.println("\n=== TOKENS ===");
         System.out.println(tokens);
 
