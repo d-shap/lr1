e3cd9e866d66a79f53541e8b54c7c00c32225376
Dmitry Shapovalov <shenf@mail.ru>
2026-08-14 22:04:35 +0300

    Поправил импорты

--- src/main/java/ru/d_shap/lr1/validator/EbnfValidator.java
+++ src/main/java/ru/d_shap/lr1/validator/EbnfValidator.java
@@ -27,17 +27,17 @@ import java.util.Map;
 import java.util.Set;
 
 import ru.d_shap.lr1.EbnfValidationException;
-import ru.d_shap.lr1.model.EbnfChoice;
-import ru.d_shap.lr1.model.EbnfExcept;
-import ru.d_shap.lr1.model.EbnfGrammar;
-import ru.d_shap.lr1.model.EbnfNode;
-import ru.d_shap.lr1.model.EbnfOptional;
-import ru.d_shap.lr1.model.EbnfReference;
-import ru.d_shap.lr1.model.EbnfRepeat;
-import ru.d_shap.lr1.model.EbnfRule;
-import ru.d_shap.lr1.model.EbnfSequence;
-import ru.d_shap.lr1.model.EbnfSpecial;
-import ru.d_shap.lr1.model.EbnfTerminal;
+import ru.d_shap.lr1.ebnf.EbnfChoice;
+import ru.d_shap.lr1.ebnf.EbnfExcept;
+import ru.d_shap.lr1.ebnf.EbnfGrammar;
+import ru.d_shap.lr1.ebnf.EbnfNode;
+import ru.d_shap.lr1.ebnf.EbnfOptional;
+import ru.d_shap.lr1.ebnf.EbnfReference;
+import ru.d_shap.lr1.ebnf.EbnfRepeat;
+import ru.d_shap.lr1.ebnf.EbnfRule;
+import ru.d_shap.lr1.ebnf.EbnfSequence;
+import ru.d_shap.lr1.ebnf.EbnfSpecial;
+import ru.d_shap.lr1.ebnf.EbnfTerminal;
 
 /**
  * The EBNF validator.
