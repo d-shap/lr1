befc6c08e23cbe42e6a640716b08deae9218d943
Dmitry Shapovalov <shenf@mail.ru>
2026-08-16 17:31:28 +0300

    Заглушка для тестов

---
+++ src/test/java/ru/d_shap/lr1/ebnf/EbnfRepeatTest.java
@@ -0,0 +1,102 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.ebnf;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.lr1.Position;
+
+/**
+ * Tests for {@link EbnfRepeat}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class EbnfRepeatTest {
+
+    /**
+     * Test class constructor.
+     */
+    public EbnfRepeatTest() {
+        super();
+    }
+
+    /**
+     * {@link EbnfRepeat} class test.
+     */
+    @Test
+    public void constructorTest() {
+        Position position = new Position(1, 1);
+        EbnfNode node = new EbnfTerminal(position, "a");
+        new EbnfRepeat(position, node, EbnfRepeatOperator.ZERO_OR_MANY);
+
+        try {
+            new EbnfRepeat(null, node, EbnfRepeatOperator.ZERO_OR_MANY);
+            Assertions.fail("EbnfRepeat test fail");
+        } catch (NullPointerException ex) {
+            Assertions.assertThat(ex).hasMessage("Position should not be null");
+        }
+        try {
+            new EbnfRepeat(position, null, EbnfRepeatOperator.ZERO_OR_MANY);
+            Assertions.fail("EbnfRepeat test fail");
+        } catch (NullPointerException ex) {
+            Assertions.assertThat(ex).hasMessage("Expression should not be null");
+        }
+        try {
+            new EbnfRepeat(position, node, null);
+            Assertions.fail("EbnfRepeat test fail");
+        } catch (NullPointerException ex) {
+            Assertions.assertThat(ex).hasMessage("Repeat operator should not be null");
+        }
+    }
+
+    /**
+     * {@link EbnfRepeat} class test.
+     */
+    @Test
+    public void getExpressionTest() {
+        // TODO
+    }
+
+    /**
+     * {@link EbnfRepeat} class test.
+     */
+    @Test
+    public void getRepeatOperatorTest() {
+        // TODO
+    }
+
+    /**
+     * {@link EbnfRepeat} class test.
+     */
+    @Test
+    public void toStringTest() {
+        // TODO
+    }
+
+    /**
+     * {@link EbnfRepeat} class test.
+     */
+    @Test
+    public void serializationTest() {
+        // TODO
+    }
+
+}
