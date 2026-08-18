406455e9c3b63808ec74bf62bbc2aeefd7032c85
Dmitry Shapovalov <shenf@mail.ru>
2026-08-18 22:16:23 +0300

    Добавил тесты на чтение

---
+++ src/test/java/ru/d_shap/lr1/source/ReaderSourceTest.java
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// LR(1) parser implementation.
+// Copyright (C) 2026 Dmitry Shapovalov.
+//
+// This file is part of LR(1) parser.
+//
+// LR(1) parser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LR(1) parser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.lr1.source;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.mock.MockReader;
+
+/**
+ * Tests for {@link ReaderSource}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ReaderSourceTest {
+
+    /**
+     * Test class constructor.
+     */
+    public ReaderSourceTest() {
+        super();
+    }
+
+    /**
+     * {@link ReaderSource} class test.
+     */
+    @Test
+    public void parseSourceTest() {
+        CharConsumer<List<String>> charConsumer = new CharConsumerImpl();
+        ReaderSource<List<String>> source = new ReaderSource<>(charConsumer);
+
+        List<String> list1 = source.parse(MockReader.builder().setContent("").buildReader());
+        Assertions.assertThat(list1).containsExactlyInOrder();
+
+        List<String> list2 = source.parse(MockReader.builder().setContent("abc").buildReader());
+        Assertions.assertThat(list2).containsExactlyInOrder("97 at 1:1", "98 at 1:2", "99 at 1:3");
+
+        List<String> list3 = source.parse(MockReader.builder().setContent("a\nbc\n  \r\n d").buildReader());
+        Assertions.assertThat(list3).containsExactlyInOrder("97 at 1:1", "10 at 1:2", "98 at 2:1", "99 at 2:2", "10 at 2:3", "32 at 3:1", "32 at 3:2", "13 at 3:3", "10 at 3:4", "32 at 4:1", "100 at 4:2");
+
+        List<String> list4 = source.parse(MockReader.builder().setContent("\n\n\n").buildReader());
+        Assertions.assertThat(list4).containsExactlyInOrder("10 at 1:1", "10 at 2:1", "10 at 3:1");
+
+        List<String> list5 = source.parse(MockReader.builder().setContent("\r\r\r").buildReader());
+        Assertions.assertThat(list5).containsExactlyInOrder("13 at 1:1", "13 at 1:2", "13 at 1:3");
+    }
+
+}
