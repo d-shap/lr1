a702fd174892c4379713de97589ac44c4ff3aa7c
Dmitry Shapovalov <shenf@mail.ru>
2026-08-21 21:39:02 +0300

    Тест на строковое представление

--- src/test/java/ru/d_shap/lr1/parser/EbnfTokenTypeTest.java
+++ src/test/java/ru/d_shap/lr1/parser/EbnfTokenTypeTest.java
@@ -45,4 +45,28 @@ public final class EbnfTokenTypeTest {
         Assertions.assertThat(EbnfTokenType.class).asEnum().hasValueCount(17);
     }
 
+    /**
+     * {@link EbnfTokenType} class test.
+     */
+    @Test
+    public void tokenTextTest() {
+        Assertions.assertThat(EbnfTokenType.IDENTIFIER.tokenText("value")).isEqualTo("IDENTIFIER(value)");
+        Assertions.assertThat(EbnfTokenType.STRING.tokenText("value")).isEqualTo("STRING(value)");
+        Assertions.assertThat(EbnfTokenType.EQUALS.tokenText("value")).isEqualTo("EQUALS");
+        Assertions.assertThat(EbnfTokenType.COMMA.tokenText("value")).isEqualTo("COMMA");
+        Assertions.assertThat(EbnfTokenType.PIPE.tokenText("value")).isEqualTo("PIPE");
+        Assertions.assertThat(EbnfTokenType.SEMICOLON.tokenText("value")).isEqualTo("SEMICOLON");
+        Assertions.assertThat(EbnfTokenType.LPAREN.tokenText("value")).isEqualTo("LPAREN");
+        Assertions.assertThat(EbnfTokenType.RPAREN.tokenText("value")).isEqualTo("RPAREN");
+        Assertions.assertThat(EbnfTokenType.LBRACKET.tokenText("value")).isEqualTo("LBRACKET");
+        Assertions.assertThat(EbnfTokenType.RBRACKET.tokenText("value")).isEqualTo("RBRACKET");
+        Assertions.assertThat(EbnfTokenType.LBRACE.tokenText("value")).isEqualTo("LBRACE");
+        Assertions.assertThat(EbnfTokenType.RBRACE.tokenText("value")).isEqualTo("RBRACE");
+        Assertions.assertThat(EbnfTokenType.QUESTION.tokenText("value")).isEqualTo("QUESTION");
+        Assertions.assertThat(EbnfTokenType.PLUS.tokenText("value")).isEqualTo("PLUS");
+        Assertions.assertThat(EbnfTokenType.MINUS.tokenText("value")).isEqualTo("MINUS");
+        Assertions.assertThat(EbnfTokenType.ASTERISK.tokenText("value")).isEqualTo("ASTERISK");
+        Assertions.assertThat(EbnfTokenType.EOF.tokenText("value")).isEqualTo("EOF");
+    }
+
 }
