2f0f4ebd2e877bd03f4c6bd6862e73d864f88f90
Dmitry Shapovalov <shenf@mail.ru>
2026-08-25 22:58:08 +0300

    Добавил тест на сериализацию

--- src/test/java/ru/d_shap/lr1/parser/EbnfTokenTest.java
+++ src/test/java/ru/d_shap/lr1/parser/EbnfTokenTest.java
@@ -22,6 +22,7 @@ package ru.d_shap.lr1.parser;
 import org.junit.Test;
 
 import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.util.SerializationHelper;
 import ru.d_shap.lr1.Position;
 
 /**
@@ -118,7 +119,14 @@ public final class EbnfTokenTest {
      */
     @Test
     public void serializationTest() {
-        // TODO
+        Position position = new Position(10, 20);
+        EbnfToken token = new EbnfToken(position, EbnfTokenType.ASTERISK, "*");
+        EbnfToken deserialized = SerializationHelper.serializeAndDeserialize(token);
+        Assertions.assertThat(deserialized.getPosition().getLine()).isEqualTo(10);
+        Assertions.assertThat(deserialized.getPosition().getColumn()).isEqualTo(20);
+        Assertions.assertThat(deserialized.getTokenType()).isEqualTo(EbnfTokenType.ASTERISK);
+        Assertions.assertThat(deserialized.getTokenValue()).isEqualTo("*");
+        Assertions.assertThat(deserialized).hasToString("ASTERISK");
     }
 
 }
